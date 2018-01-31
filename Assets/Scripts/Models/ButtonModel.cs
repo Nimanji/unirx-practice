d8d8469c22b68bc000a15f6f85a8ccff882a1b9c
Nimanji <terataka.ggl@gmail.com>
2018-02-01 00:28:40 +0900

    SimpleButton-UpDown
    presenterからmodelの値の操作を行い、かつmodelで変更のあったReactivePropertyの信号をpresenterでキャッチするよう処理。

---
+++ Assets/Scripts/Models/ButtonModel.cs
@@ -0,0 +1,39 @@
+﻿// ==============================
+// @author Nimanji (Indies a.k.a)
+// ==============================
+
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+// ==============================
+// ButtonModel
+// ==============================
+namespace Assets.Scripts.Models
+{
+    public class ButtonModel
+    {
+        // プロパティ
+        public ReactiveProperty<int> push_count {get; private set;}
+
+        // コンストラクタ
+        public ButtonModel ()
+        {
+            this.push_count = new ReactiveProperty<int>();
+            this.push_count.Value = 0;
+        }
+
+        // カウントを加算する
+        public void upCounter ()
+        {
+            this.push_count.Value++;
+        }
+
+        // カウントを減少する
+        public void downCounter ()
+        {
+            this.push_count.Value--;
+        }
+    }
+}
---
+++ Assets/Scripts/Presenters/ButtonPresenter.cs
@@ -0,0 +1,44 @@
+﻿// ==============================
+// @author Nimanji (Indies a.k.a)
+// ==============================
+
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Assets.Scripts.Models;
+
+// ==============================
+// ButtonPresenter
+// ==============================
+namespace Assets.Scripts.Presenters
+{
+    public class ButtonPresenter : MonoBehaviour
+    {
+        // View (UI)
+        public Button up_button;
+        public Button down_button;
+        public Text count_text;
+
+        // Model
+        private ButtonModel _btn_model;
+
+        // start
+        void Start ()
+        {
+            // Model登録
+            this._btn_model = new ButtonModel();
+            // 加算ボタン押下時のアクション作成
+            this.up_button.OnClickAsObservable()
+                .Subscribe(_ => this._btn_model.upCounter());
+            // 減少ボタン押下時のアクション作成
+            this.down_button.OnClickAsObservable()
+                .Subscribe(_ => this._btn_model.downCounter());
+            // ButtonModelのpush_countが変化したらテキストを更新
+            this._btn_model.push_count
+                .Subscribe(num => this.count_text.text = num.ToString());
+        }
+    }
+}
